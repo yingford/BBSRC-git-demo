cacd3e90d9b03eed33fc4f2fa91b408f3b4e6a55
yingford <yglingen@gmail.com>
2017-04-10 10:24:51 +0100

    adding outlier removal

--- get_Lpaper.R
+++ get_Lpaper.R
@@ -5,8 +5,20 @@ df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/Lskyeta
               header=TRUE,sep=',')
 head(df)
 
+max_age=120
+min_age=18
+
+# clean data
+df=subset(df,age>min_age&age<max_age)
+
+# sanity check with smoke test
+
+stopifnot(max(df$age)<max_age)
+stopifnot(min(df$age)>min_age)
+
 # run linear fit: lm
 lm.result=lm(conspiracist_avg~age,data=df)
 summary(lm.result)
 
 plot(df$age,df$conspiracist_avg)
+
