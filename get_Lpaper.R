6263a4c4467eb314a87962c00acf8dc67fd665a6
yingford <yglingen@gmail.com>
2017-04-10 10:18:01 +0100

    adding plot

--- get_Lpaper.R
+++ get_Lpaper.R
@@ -1,4 +1,12 @@
-# R code: this code reads Lewandowsky's dataset into RStudio
+# R code: this code 
+
+# read Lewandowsky's dataset into RStudio
 df=read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/LskyetalPLOSONE.csv',
               header=TRUE,sep=',')
 head(df)
+
+# run linear fit: lm
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
+
+plot(df$age,df$conspiracist_avg)
